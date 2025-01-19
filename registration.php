8865bbda413bb5ced3725924f2b7bfdf15215147
Ankit Dhiman <ankitdhiman@Ankits-MacBook-Air.local>
2025-01-19 21:40:07 +0530

    today i am creating the forms of task management and style them with css and bootstrap

---
+++ registration.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div class="row">
+        <div class="col-md-3 mt-5 m-auto" id="userlogin">
+            <center><h1 id="userloginheading">User Registration</h1></center><br>
+            <form action="" method="post">
+            <div class="form-group">
+                    Enter your username : <input type="text" name="nm" class="form-control" required><br>
+                </div>
+                <div class="form-group">
+                    Enter your email : <input type="email" name="em" class="form-control" required><br>
+                </div>
+                <div class="form-group">
+                    Enter your password : <input type="password" name="pass" class="form-control" required><br>
+                </div>   
+                <div class="form-group">
+                    Enter your mobile : <input type="number" name="mob" class="form-control" required><br>
+                </div> 
+                <div class="form-group">
+                    <center>
+                    <input type="submit" name="userreg" value="Register"  class="btn btn-warning"> &nbsp 
+                    <a href="taskmanagement.php" class="btn btn-warning">Go Back</a><br><br>
+                    <a href="user_login.php" class="btn btn-success">Already have an account.</a>
+
+
+                    </center>
+                    
+                </div>
+               
+            </form>
+        </div>
+    </div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>    
+</body>
+</html>
---
+++ taskmanagement.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+        <div class="row">
+            <div class="col-md-4 mt-5 m-auto">
+                <center>
+                    <h1>Task management System</h1><br><br>
+                    <h3>Choose Login Role ?</h3><br><br>
+                    <a href="User_login.php" class=" btn btn-success">User Login</a> &nbsp
+                    <a href="registration.php" class=" btn btn-success">User Registration </a> &nbsp
+                    <a href="admin.php" class=" btn btn-success">Admin Login</a> 
+                </center>
+            </div>
+        </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
